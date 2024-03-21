ef32cd1a1f7dd240cdd6dc2c9dbc8efa72abc043
Aniketh Pratihar <aniketh5660@gmail.com>
2024-03-21 14:25:16 +0530

    None

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void main() {
 
 }
 
-class MyApp  extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
